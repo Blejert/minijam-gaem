ba92f5c165908cafbbf22f2563f27e52e36ed3b9
theboredkid132 <122572302+theboredkid132@users.noreply.github.com>
2023-08-18 16:47:27 -0600

    Update collisions.py

--- collisions.py
+++ collisions.py
@@ -2,11 +2,13 @@
 # will get massive changes and will have to get the terrain data to check if the player is next to the ground
 
 def nextto_down(player_xy):
-    if player_xy[1] + 100 + 1 > 800:
+    if player_xy[1] + 71 > 675:
         return True
     return False
 
 def nextto_up(player_xy):
+    if player_xy[1] - 1 < 0:
+        return True
     return False
 
 def nextto_left(player_xy):
@@ -15,6 +17,6 @@ def nextto_left(player_xy):
     return False
 
 def nextto_right(player_xy):
-    if player_xy[0] + 50 + 1 > 1000:
+    if player_xy[0] + 35 + 1 > 1275:
         return True
     return False
