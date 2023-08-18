b41cbf56eb6c8fd354dd2275831f3ffbf0accadb
theboredkid132 <122572302+theboredkid132@users.noreply.github.com>
2023-08-18 16:34:05 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,25 @@ def main():
     import pygame
     import random
     from collisions import nextto_down, nextto_up, nextto_left, nextto_right
-    import texture_loading as tl
 
-    screen = pygame.display.set_mode((1000,800))
+    WIDTH, HEIGHT = 1275, 675
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    # art
+    player = pygame.transform.scale(pygame.image.load("art/tile002.png"), (35, 70))
+    player_flippedX = pygame.transform.flip(player, True, False)
+    player_flippedY = pygame.transform.flip(player, False, True)
+    player_flippedYX = pygame.transform.flip(player, True, True)
 
     clock = pygame.time.Clock()
 
-    player = tl.player
     player_xy = [0, 0]
     player_y_vel = 0
 
+    # veriables
     jumpsLeft = 0
     flip = False
+    playerFlipX = False
 
     while True:
         for e in pygame.event.get():
@@ -21,49 +28,58 @@ def main():
                 pygame.quit()
                 exit()
 
-        screen.fill(pygame.Color(0,0,0))
-        screen.blit(player, (player_xy[0], player_xy[1]))
+        screen.fill(pygame.Color(0,0,200))
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_d] and not nextto_right(player_xy):
             player_xy[0] += 5
+            playerFlipX = False
         if keys[pygame.K_a] and not nextto_left(player_xy):
             player_xy[0] -= 5
+            playerFlipX = True
 
         if not flip:
             if nextto_down(player_xy):
                 jumpsLeft = 2
             if keys[pygame.K_w] or keys[pygame.K_SPACE]:
-                if nextto_down(player_xy):
-                    player_y_vel = -10
-                    jumpsLeft -= 1
-                    jumped = True
-                if not jumped and jumpsLeft == 1 and not nextto_up(player_xy):
-                    jumpsLeft = 0
-                    jumped = True
-                    if random.randint(0, 2) == 1:
-                        player_y_vel = -10
-                    else:
-                        player_y_vel = 10
-                        flip = True
+                if not jumped and not nextto_up(player_xy):
+                    if jumpsLeft > 0:
+                        if jumpsLeft == 1:
+                            if random.randint(0, 2) == 1:
+                                player_y_vel = -10
+                                jumpsLeft -= 1
+                                jumped = True
+                            else:
+                                player_y_vel = 10
+                                flip = True
+                                jumpsLeft -= 1
+                                jumped = True
+                        else:
+                            player_y_vel = -10
+                            jumpsLeft -= 1
+                            jumped = True
             else:
                 jumped = False
-        else:
+        elif flip:
             if nextto_up(player_xy):
                 jumpsLeft = 2
             if keys[pygame.K_w] or keys[pygame.K_SPACE]:
-                if nextto_up(player_xy):
-                    player_y_vel = 10
-                    jumpsLeft -= 1
-                    jumped = True
-                if not jumped and jumpsLeft == 1 and not nextto_down(player_xy):
-                    jumpsLeft = 0
-                    jumped = True
-                    if random.randint(0, 2) == 1:
-                        player_y_vel = 10
-                    else:
-                        player_y_vel = -10
-                        flip = False
+                if not jumped and not nextto_down(player_xy):
+                    if jumpsLeft > 0:
+                        if jumpsLeft == 1:
+                            if random.randint(0, 2) == 1:
+                                player_y_vel = 10
+                                jumpsLeft -= 1
+                                jumped = True
+                            else:
+                                player_y_vel = -10
+                                flip = False
+                                jumpsLeft -= 1
+                                jumped = True
+                        else:
+                            player_y_vel = 10
+                            jumpsLeft -= 1
+                            jumped = True
             else:
                 jumped = False
 
@@ -85,10 +101,20 @@ def main():
             elif player_y_vel > 0:
                 player_y_vel = 0
 
+        if not playerFlipX:
+            if flip:
+                screen.blit(player_flippedY, (player_xy[0], player_xy[1]))
+            else:
+                screen.blit(player, (player_xy[0], player_xy[1]))
+        else:
+            if flip:
+                screen.blit(player_flippedYX, (player_xy[0], player_xy[1]))
+            else:
+                screen.blit(player_flippedX, (player_xy[0], player_xy[1]))
+
         print(jumpsLeft)
         pygame.display.update()
         clock.tick(60)
-
 
 if __name__ == "__main__":
     main()
