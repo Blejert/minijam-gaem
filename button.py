446db8074876614f8c6a4f8c76328f8dc577c2f8
theboredkid132 <122572302+theboredkid132@users.noreply.github.com>
2023-08-19 17:48:31 -0600

    Add files via upload

---
+++ button.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+# Set up the font
+font = pygame.font.SysFont('Tahoma', 50)
+
+
+def draw_button(WIN, x, y, button_sprite):
+    WIN.blit(button_sprite, (x, y))
+
+
+# Function to check if the button is clicked
+def is_button_clicked(button_x, button_y, button_sprite):
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    button_rect = button_sprite.get_rect()
+    button_rect.topleft = (button_x, button_y)
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN and button_rect.collidepoint(mouse_x, mouse_y):
+            return True
