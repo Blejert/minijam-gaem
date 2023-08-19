9ab5da8c55a8c7c832a436710f22aee628e8f715
theboredkid132 <122572302+theboredkid132@users.noreply.github.com>
2023-08-19 09:02:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,12 @@
 import pygame
-import random
 import math
-from collisions import nextto_down, nextto_up, nextto_left, nextto_right
+pygame.font.init()
 from animation import animation
-pygame.font.init()
-pygame.mixer.init()
-
-pygame.font.init()
-
-WIDTH, HEIGHT = 1200, 600
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Coin Jump")
 
 
-# variables #
-PLAYER_WIDTH = 50
-PLAYER_HEIGHT = 50
-PLAYER_VEL = 5
-level = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
-player_xy = [0, 0]
-player_y_vel = 0
-jumpsLeft = 0
-flip = False
-playerFlipX = False
-frame = 0
-###
+def draw(flip, WIDTH, HEIGHT, WIN, level, jumpsLeft, frame, player_xy, playerFlipX):
+    FONT = pygame.font.SysFont("", 30)
 
-FONT = pygame.font.SysFont("", 30)
-
-# art #
-player = pygame.transform.scale(pygame.image.load("art/tile002.png"), (35, 70))
-player_flippedX = pygame.transform.flip(player, True, False)
-player_flippedY = pygame.transform.flip(player, False, True)
-player_flippedYX = pygame.transform.flip(player, True, True)
-
-background = pygame.transform.scale(pygame.image.load("art/tile019.png"), (40, 40))
-grass = pygame.transform.scale(pygame.image.load("art/tile016.png"), (40, 40))
-
-coin = pygame.transform.scale(pygame.image.load("art/coin/tile000.png"), (35, 35))
-coin1 = pygame.transform.scale(pygame.image.load("art/coin/tile001.png"), (35, 35))
-coin2 = pygame.transform.scale(pygame.image.load("art/coin/tile002.png"), (35, 35))
-coin3 = pygame.transform.scale(pygame.image.load("art/coin/tile003.png"), (35, 35))
-###
-
-# MusicAndSounds/Sounds #
-pygame.mixer.music.load('MusicAndSounds/Min_Jam_Fae_Dark.wav')
-pygame.mixer.music.set_volume(0.3)
-pygame.mixer.music.play(-1)
-###
-
-
-
-def draw(player):
-    global next_frame_time, current_frame, frame, playCoin
     WIN.fill('white')
 
     # background #
@@ -80,6 +20,8 @@ def draw(player):
         for y in range (15):
             if level[y][x] == 1:
                 WIN.blit(grass, (x * 40, y * 40))
+            elif level[y][x] == 2:
+                WIN.blit(dirt, (x * 40, y * 40))
 
     # text #
     if flip:
@@ -116,106 +58,3 @@ def draw(player):
     ###
 
     pygame.display.update()
-
-
-def main():
-    # stupid global stuff #
-    global player_y_vel
-    global flip
-    global jumpsLeft
-    global jumped
-    global playerFlipX
-    ###
-
-    run = True
-
-    clock = pygame.time.Clock()
-
-    while run:
-        clock.tick(60)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                break
-
-        # Player Movement #
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_d] and not nextto_right(player_xy, level):
-            player_xy[0] += 5
-            playerFlipX = False
-        if keys[pygame.K_a] and not nextto_left(player_xy, level):
-            player_xy[0] -= 5
-            playerFlipX = True
-        ###
-
-        # jump #
-        if not flip:
-            if nextto_down(player_xy, level):
-                jumpsLeft = 2
-            if keys[pygame.K_w] or keys[pygame.K_SPACE]:
-                if nextto_down(player_xy, level):
-                    player_y_vel = -10
-                    jumpsLeft -= 1
-                    jumped = True
-                if not jumped and jumpsLeft == 1:
-                    jumpsLeft = 0
-                    jumped = True
-                    if random.randint(0, 2) == 1:
-                        player_y_vel = -10
-                    else:
-                        player_y_vel = 10
-                        flip = True
-            else:
-                jumped = False
-        else:
-            if nextto_up(player_xy, level):
-                jumpsLeft = 2
-            if keys[pygame.K_w] or keys[pygame.K_SPACE]:
-                if nextto_up(player_xy, level):
-                    player_y_vel = 10
-                    jumpsLeft -= 1
-                    jumped = True
-                if not jumped and jumpsLeft == 1:
-                    jumpsLeft = 0
-                    jumped = True
-                    if random.randint(0, 2) == 1:
-                        player_y_vel = 10
-                    else:
-                        player_y_vel = -10
-                        flip = False
-            else:
-                jumped = False
-        ###
-
-        # gravity #
-        if not flip:
-            if not nextto_down(player_xy, level):
-                player_y_vel += 0.3
-        else:
-            if not nextto_up(player_xy, level):
-                player_y_vel -= 0.3
-        ###
-
-        # bumping into the roof #
-        if nextto_down((player_xy[0], player_xy[1] - (20 if flip else 0)), level) and player_y_vel > 0:
-            player_y_vel = 0
-        if nextto_up((player_xy[0], player_xy[1] + (20 if not flip else 0)), level) and player_y_vel < 0:
-            player_y_vel = 0
-
-        # velocity I think #
-        for _ in range(round(player_y_vel)):
-            if player_y_vel > 0 and not nextto_down((player_xy[0], player_xy[1] - (20 if flip else 0)), level):
-                player_xy[1] += 1
-        for _ in range(-round(player_y_vel)):
-            if player_y_vel < 0 and not nextto_up((player_xy[0], player_xy[1] + (20 if not flip else 0)), level):
-                player_xy[1] -= 1
-        ###
-
-        draw(player)
-
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
