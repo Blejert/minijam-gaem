b59845bdf22918f395b5dd0fa9bca5af246935f9
theboredkid132 <122572302+theboredkid132@users.noreply.github.com>
2023-08-19 13:50:07 -0600

    Update collisions.py

--- collisions.py
+++ collisions.py
@@ -1,21 +1,31 @@
 # functions check if on a given direction the player is right next to something like the ground or the edge of the screen
-# dont try to understand how it works, just ask me if it breaks
+# don't try to understand how it works, just ask me if it breaks
+# I somehow understood it
+
 import math
+from Draw import die
 
 
 def nextto_down(player_xy, level):
     if player_xy[1] + 70 + 1 > 600:
         return True
-    if ( player_xy[1] + 70 ) % 40 == 0:
+    if (player_xy[1] + 70) % 40 == 0:
+        if level[math.floor((player_xy[1] + 70) / 40)][math.floor(player_xy[0] / 40)] == 3:
+            die()
+    if (player_xy[1] + 70) % 40 == 0:
         if level[math.floor((player_xy[1] + 70) / 40)][math.floor(player_xy[0] / 40)] != 0 or level[math.floor((player_xy[1] + 70) / 40)][math.floor((player_xy[0] + 34) / 40)] != 0:
             return True
+
     return False
 
 
 def nextto_up(player_xy, level):
     if player_xy[1] - 1 < 0:
         return True
-    if ( player_xy[1] - 40 ) % 40 == 0:
+    if (player_xy[0] - 40) % 40 == 0:
+        if level[math.floor((player_xy[1] - 40) / 40)][math.floor(player_xy[0] / 40)] == 4:
+            die()
+    if (player_xy[1] - 40) % 40 == 0:
         if level[math.floor((player_xy[1] - 40) / 40)][math.floor(player_xy[0] / 40)] != 0 or level[math.floor((player_xy[1] - 40) / 40)][math.floor((player_xy[0] + 34) / 40)] != 0:
             return True
     return False
