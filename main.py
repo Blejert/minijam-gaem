ba421f2d85ced1703991fc2417b6f8f7cc0b3c0e
theboredkid132 <122572302+theboredkid132@users.noreply.github.com>
2023-08-19 09:03:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,151 @@
 import pygame
-import math
+import random
+from Draw import draw
+from collisions import nextto_down, nextto_up, nextto_left, nextto_right
 pygame.font.init()
-from animation import animation
+pygame.mixer.init()
+
+WIDTH, HEIGHT = 1200, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Coin Jump")
 
 
-def draw(flip, WIDTH, HEIGHT, WIN, level, jumpsLeft, frame, player_xy, playerFlipX):
-    FONT = pygame.font.SysFont("", 30)
+# variables #
+PLAYER_WIDTH = 50
+PLAYER_HEIGHT = 50
+PLAYER_VEL = 5
+level = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+player_xy = [0, 0]
+player_y_vel = 0
+jumpsLeft = 0
+flip = False
+playerFlipX = False
+frame = 0
+###
 
-    WIN.fill('white')
+# MusicAndSounds/Sounds #
+pygame.mixer.music.load('MusicAndSounds/Min_Jam_Fae_Dark.wav')
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play(-1)
+###
 
-    # background #
-    for x in range(math.ceil(WIDTH / 40)):
-        for y in range(math.ceil(HEIGHT / 40)):
-            WIN.blit(background, (x * 40, y * 40))
+
+
+
+
+
+def main():
+    # stupid global stuff #
+    global player_y_vel
+    global flip
+    global jumpsLeft
+    global jumped
+    global playerFlipX
     ###
 
-    # level #
-    for x in range(30):
-        for y in range (15):
-            if level[y][x] == 1:
-                WIN.blit(grass, (x * 40, y * 40))
-            elif level[y][x] == 2:
-                WIN.blit(dirt, (x * 40, y * 40))
+    run = True
 
-    # text #
-    if flip:
-        hi_text = FONT.render("Tails", True, "white")
-        WIN.blit(hi_text, (WIDTH - 60, 45))
-    else:
-        hi_text = FONT.render("Heads", True, "white")
-        WIN.blit(hi_text, (WIDTH - 70, 45))
-    ###
+    clock = pygame.time.Clock()
 
-    # misc #
-    if not jumpsLeft > 0:
-        if frame < 8: #<- this animation has 4 frames, so I play it 2 times
-            frame = animation(WIN, [coin, coin1, coin2, coin3], (WIDTH - 55, 0), 100, False)
+    while run:
+        clock.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        # Player Movement #
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_d] and not nextto_right(player_xy, level):
+            player_xy[0] += 5
+            playerFlipX = False
+        if keys[pygame.K_a] and not nextto_left(player_xy, level):
+            player_xy[0] -= 5
+            playerFlipX = True
+        ###
+
+        # jump #
+        if not flip:
+            if nextto_down(player_xy, level):
+                jumpsLeft = 2
+            if keys[pygame.K_w] or keys[pygame.K_SPACE]:
+                if nextto_down(player_xy, level):
+                    player_y_vel = -10
+                    jumpsLeft -= 1
+                    jumped = True
+                if not jumped and jumpsLeft == 1:
+                    jumpsLeft = 0
+                    jumped = True
+                    if random.randint(0, 2) == 1:
+                        player_y_vel = -10
+                    else:
+                        player_y_vel = 10
+                        flip = True
+            else:
+                jumped = False
         else:
-            animation(WIN, [coin, coin1, coin2, coin3], (WIDTH - 55, 0), 100, True)
-            WIN.blit(coin, (WIDTH - 55, 0))
-    else:
-        frame = 0
-        WIN.blit(coin, (WIDTH - 55, 0))
-    ###
+            if nextto_up(player_xy, level):
+                jumpsLeft = 2
+            if keys[pygame.K_w] or keys[pygame.K_SPACE]:
+                if nextto_up(player_xy, level):
+                    player_y_vel = 10
+                    jumpsLeft -= 1
+                    jumped = True
+                if not jumped and jumpsLeft == 1:
+                    jumpsLeft = 0
+                    jumped = True
+                    if random.randint(0, 2) == 1:
+                        player_y_vel = 10
+                    else:
+                        player_y_vel = -10
+                        flip = False
+            else:
+                jumped = False
+        ###
 
-    # player #
-    if not playerFlipX:
-        if flip:
-            WIN.blit(player_flippedY, (player_xy[0], player_xy[1]))
+        # gravity #
+        if not flip:
+            if not nextto_down(player_xy, level):
+                player_y_vel += 0.3
         else:
-            WIN.blit(player, (player_xy[0], player_xy[1]))
-    else:
-        if flip:
-            WIN.blit(player_flippedYX, (player_xy[0], player_xy[1]))
-        else:
-            WIN.blit(player_flippedX, (player_xy[0], player_xy[1]))
-    ###
+            if not nextto_up(player_xy, level):
+                player_y_vel -= 0.3
+        ###
 
-    pygame.display.update()
+        # bumping into the roof #
+        if nextto_down((player_xy[0], player_xy[1] - (20 if flip else 0)), level) and player_y_vel > 0:
+            player_y_vel = 0
+        if nextto_up((player_xy[0], player_xy[1] + (20 if not flip else 0)), level) and player_y_vel < 0:
+            player_y_vel = 0
+
+        # velocity I think #
+        for _ in range(round(player_y_vel)):
+            if player_y_vel > 0 and not nextto_down((player_xy[0], player_xy[1] - (20 if flip else 0)), level):
+                player_xy[1] += 1
+        for _ in range(-round(player_y_vel)):
+            if player_y_vel < 0 and not nextto_up((player_xy[0], player_xy[1] + (20 if not flip else 0)), level):
+                player_xy[1] -= 1
+        ###
+
+        draw(flip, WIDTH, HEIGHT, WIN, level, jumpsLeft, frame, player_xy, playerFlipX)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
