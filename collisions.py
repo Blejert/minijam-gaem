4789022b11edf66366c3cc4fcd95a0a0a51ab2fc
theboredkid132 <122572302+theboredkid132@users.noreply.github.com>
2023-08-19 21:10:22 -0600

    Update collisions.py

--- collisions.py
+++ collisions.py
@@ -2,12 +2,15 @@
 # don't try to understand how it works, just ask me if it breaks
 # I somehow understood it
 
+import pygame
 import math
 import Draw
 
+Death_Sound = pygame.mixer.Sound("MusicAndSounds/death.wav")
+Death_Sound.set_volume(0.5)
 
-def move(player_xy):
-    player_xy = [0, 450]
+Next_Sound = pygame.mixer.Sound("MusicAndSounds/nextlevel.wav")
+Next_Sound.set_volume(0.5)
 
 
 def nextto_down(player_xy, level):
@@ -15,6 +18,7 @@ def nextto_down(player_xy, level):
         return True
     if (player_xy[1] + 70) % 40 == 0:
         if level[math.floor((player_xy[1] + 70) / 40)][math.floor(player_xy[0] / 40)] == 3:
+            Death_Sound.play()
             Draw.dead = True
     if (player_xy[1] + 70) % 40 == 0:
         if level[math.floor((player_xy[1] + 70) / 40)][math.floor(player_xy[0] / 40)] != 0 or level[math.floor((player_xy[1] + 70) / 40)][math.floor((player_xy[0] + 34) / 40)] != 0:
@@ -27,7 +31,8 @@ def nextto_up(player_xy, level):
     if player_xy[1] - 1 < 0:
         return True
     if (player_xy[0] - 40) % 40 == 0:
-        if level[math.floor((player_xy[1] - 40) / 40)][math.floor(player_xy[0] / 40)] == 4:
+        if level[math.floor((player_xy[1] - 20) / 40)][math.floor(player_xy[0] / 40)] == 4:
+            Death_Sound.play()
             Draw.dead = True
     if (player_xy[1] - 40) % 40 == 0:
         if level[math.floor((player_xy[1] - 40) / 40)][math.floor(player_xy[0] / 40)] != 0 or level[math.floor((player_xy[1] - 40) / 40)][math.floor((player_xy[0] + 34) / 40)] != 0:
@@ -55,6 +60,6 @@ def nextto_right(player_xy, level):
 
 def nexttolevel_right(player_xy):
     if player_xy[0] + 35 + 1 > 1200:
+        Next_Sound.play()
         return True
     return False
-
