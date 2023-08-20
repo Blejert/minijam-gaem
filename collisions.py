04d89ac9f6bf13c3efeaab5fff3982c6e78ab2e6
theboredkid132 <122572302+theboredkid132@users.noreply.github.com>
2023-08-19 21:26:00 -0600

    Update collisions.py

--- collisions.py
+++ collisions.py
@@ -1,6 +1,6 @@
 # functions check if on a given direction the player is right next to something like the ground or the edge of the screen
 # don't try to understand how it works, just ask me if it breaks
-# I somehow understood it
+
 
 import pygame
 import math
@@ -21,7 +21,8 @@ def nextto_down(player_xy, level):
             Death_Sound.play()
             Draw.dead = True
     if (player_xy[1] + 70) % 40 == 0:
-        if level[math.floor((player_xy[1] + 70) / 40)][math.floor(player_xy[0] / 40)] != 0 or level[math.floor((player_xy[1] + 70) / 40)][math.floor((player_xy[0] + 34) / 40)] != 0:
+        if level[math.floor((player_xy[1] + 70) / 40)][math.floor(player_xy[0] / 40)] != 0 or \
+                level[math.floor((player_xy[1] + 70) / 40)][math.floor((player_xy[0] + 34) / 40)] != 0:
             return True
 
     return False
@@ -35,7 +36,8 @@ def nextto_up(player_xy, level):
             Death_Sound.play()
             Draw.dead = True
     if (player_xy[1] - 40) % 40 == 0:
-        if level[math.floor((player_xy[1] - 40) / 40)][math.floor(player_xy[0] / 40)] != 0 or level[math.floor((player_xy[1] - 40) / 40)][math.floor((player_xy[0] + 34) / 40)] != 0:
+        if level[math.floor((player_xy[1] - 40) / 40)][math.floor(player_xy[0] / 40)] != 0 or \
+                level[math.floor((player_xy[1] - 40) / 40)][math.floor((player_xy[0] + 34) / 40)] != 0:
             return True
     return False
 
@@ -48,11 +50,10 @@ def nextto_left(player_xy, level):
             return True
     return False
 
-
 def nextto_right(player_xy, level):
-    if player_xy[0] + 40 + 1 > 1200:
+    if player_xy[0] + 35 + 1 > 1200:
         return True
-    if ( player_xy[0] + 40 ) % 40 == 0:
+    if ( player_xy[0] + 35 ) % 40 == 0:
         if level[math.floor(player_xy[1] / 40)][math.floor((player_xy[0] + 35) / 40)] != 0 or level[math.floor((player_xy[1] + 35) / 40)][math.floor((player_xy[0] + 35) / 40)] != 0 or level[math.floor((player_xy[1] + 69) / 40)][math.floor((player_xy[0] + 35) / 40)] != 0:
             return True
     return False
